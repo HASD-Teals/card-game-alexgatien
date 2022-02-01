a74b9837f5c538da341499f79aa8231aa492982b
alexgatien <alexgatien14@gmail.com>
2022-02-01 09:23:55 -0600

    1-7 Card Project

--- src/Card.java
+++ src/Card.java
@@ -2,9 +2,21 @@ public class Card {
     // PROPERTIES
     private String color;
     private char rank;
+    private String symbol;
+    private int value;
+    private boolean playable;
 
     // CONSTRUCTORS
-    public Card() {}
+    public Card() {
+    }
+
+    public Card(String color,char rank, String symbol, int value, boolean playable) {
+        this.color = color;
+        this.rank = rank;
+        this.symbol = symbol;
+        this.value = value;
+        this.playable = playable;
+    }
 
     // ACCESSORS
     public String getColor() {
@@ -14,7 +26,15 @@ public class Card {
     public char getRank() {
         return this.rank;
     }
-
+    public String getSymbol(){
+        return this.symbol;
+    }
+    public int getValue(){
+        return this.value;
+    }
+    public boolean getPlayable(){
+        return this.playable;
+    }
     // MUTATORS
     public void setColor(String color) {
         this.color = color;
@@ -23,6 +43,17 @@ public class Card {
     public void setRank(char rank) {
         this.rank = rank;
     }
+    public void setValue(int value){
+        this.value = value;
+    }
+    public void setPlayable(boolean playable){
+        this.playable = playable;
+    }
 
     // METHODS
+
+
+
+
 }
+
